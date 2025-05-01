bad1cbc1b626ce19cfdf1eec19c475fe9cd160dc
royjeong1116 <royjeong1116@gmail.com>
2025-05-01 14:49:09 +0900

    update database import

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql://royjeong1116:4321@localhost:5432/project"
